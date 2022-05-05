e4c2202a05330b5707d8b7591de857f7db563048
crazydrummer81 <crazy_drummer@mail.ru>
2022-05-05 06:57:29 +0600

    Lesson 72. Added slorts

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,44 @@
 <template>
-	<h3>Hey!</h3>
-	<greeting-message :age="age"/>
-	<user-card :age="age" @age-change="onEmitChange"/>
+	<div v-if="0">
+		<h3>Hey!</h3>
+		<greeting-message :age="age"/>
+		<user-card :age="age" @age-change="onEmitChange"/>
+	</div>
+	<app-form>
+		<template v-slot:help>
+			<h2>Sign in</h2>
+		</template>
+		<template v-slot:fields>
+			<input type="email" placeholder="Email" autocomplete="on">
+			<input type="text" placeholder="User name" autocomplete="on">
+			<input type="password" placeholder="Password" autocomplete="on">
+		</template>
+		<template vslot:buttons>
+			<button type="submit">Submit</button>
+		</template>
+	</app-form>
+
+	<app-form>
+		<template v-slot:help>
+			<h2>Contact</h2>
+		</template>
+		<template v-slot:fields>
+			<input type="email" placeholder="Name" autocomplete="on">
+			<input type="text" placeholder="Message" autocomplete="off">
+		</template>
+		<template vslot:buttons>
+			<button type="submit">Submit</button>
+		</template>
+	</app-form>
 </template>
 
 <script>
 import GreetingMessage from '@/components/GreetingMessage'
 import UserCard from '@/components/UserCard'
+import AppForm from '@/components/AppForm'
 export default {
 	name: 'App',
-	components: { GreetingMessage, UserCard },
+	components: { GreetingMessage, UserCard, AppForm },
 	data() {
 		return {
 			age: 20
